8baa5fa2499aabe18576cd17253b25b6980dd854
tnaybabua <rahultanaybabu@gmail.com>
2026-03-18 09:51:00 +0530

    Gokul Added feature/UC2-BasicRoomTypes

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,11 +1,66 @@
+abstract class Room {
+
+    String roomType;
+    int beds;
+    double price;
+
+    Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println(" Welcome to Book My Stay Application ");
-        System.out.println(" Hotel Booking System Version 1.0 ");
-        System.out.println("=====================================");
+        System.out.println("====== Book My Stay Application v2.0 ======");
 
+        Room single = new SingleRoom();
+        Room dbl = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailable);
+        System.out.println();
+
+        dbl.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailable);
     }
 }
